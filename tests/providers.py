58e42a6a5abb3e831fb18997a2a11bb7597ca5f7
topilski <atopilski@gmail.com>
2020-08-30 05:26:43 -0400

    Tests

--- tests/providers.py
+++ tests/providers.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
-import datetime
 import unittest
 
-from pymodm import connect
-
+from pyfastocloud_models.provider.entry import Provider
 from pyfastocloud_models.service.entry import ServiceSettings, HostAndPort
-from pyfastocloud_models.stream.entry import ProxyStream, OutputUrl
 
 
-class StreamsTest(unittest.TestCase):
+class Providers(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        super(StreamsTest, self).__init__(*args, **kwargs)
-        connect(mongodb_uri='mongodb://localhost:27017/iptv')
+        super(Providers, self).__init__(*args, **kwargs)
 
-    def test_proxy(self):
-        now = datetime.datetime.utcnow()
-        stable = int(now.microsecond / 1000) * 1000
-        now = now.replace(microsecond=stable)
-        msec = int(now.timestamp() * 1000)
-        output_url = OutputUrl(id=OutputUrl.generate_id(), uri='test')  # required
-        name = 'Test'  # required
-        proxy_data = {ProxyStream.NAME_FIELD: name, ProxyStream.GROUPS_FIELD: ['Movies', 'USA'],
-                      ProxyStream.PRICE_FIELD: 1.1,
-                      ProxyStream.CREATED_DATE_FIELD: msec, ProxyStream.OUTPUT_FIELD: [output_url.to_front_dict()]}
-        proxy = ProxyStream.make_entry(proxy_data)
-        proxy.save()
+    def test_provider_add_server(self):
+        provider = Provider.make_provider(email='test@test.com', first_name='Alex', last_name='Top', password='123',
+                                          country='USA', language='en')
+        self.assertTrue(provider.is_valid())
+        provider.add_server(None)
+        self.assertEqual(provider.servers, [])
 
         name_server = 'Server'
         host = HostAndPort(host='localhost', port=1234)
@@ -55,24 +45,10 @@ class StreamsTest(unittest.TestCase):
                                              ServiceSettings.PROXY_DIRECTORY_FIELD: proxy_dir,
                                              ServiceSettings.MONITORING_FILED: monitoring,
                                              ServiceSettings.AUTO_START_FIELD: auto_start})
-        server.add_stream(proxy)
-        self.assertEqual(len(server.streams), 1)
-        server.remove_stream(proxy)
-        self.assertEqual(len(server.streams), 0)
-        self.assertEqual(server.name, name_server)
-        self.assertEqual(server.host, host)
-        self.assertEqual(server.http_host, http_host)
-        self.assertEqual(server.vods_host, vods_host)
-        self.assertEqual(server.cods_host, cods_host)
-        self.assertEqual(server.feedback_directory, feed)
-        self.assertEqual(server.timeshifts_directory, timeshift_dir)
-        self.assertEqual(server.hls_directory, hls_dir)
-        self.assertEqual(server.vods_directory, vods_dir)
-        self.assertEqual(server.cods_directory, cods_dir)
-        self.assertEqual(server.proxy_directory, proxy_dir)
-        self.assertEqual(server.monitoring, monitoring)
-        self.assertEqual(server.auto_start, auto_start)
         self.assertTrue(server.is_valid())
+        provider.add_server(server)
+        self.assertTrue(provider.servers, [server])
 
-    if __name__ == '__main__':
-        unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()

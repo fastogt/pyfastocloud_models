84163e9cff9010e09c1f56ed6d26cef9a7f181a2
topilski <atopilski@gmail.com>
2020-09-07 06:44:18 -0400

    To front for pairs

--- pyfastocloud_models/provider/entry_pair.py
+++ pyfastocloud_models/provider/entry_pair.py
@@ -28,4 +28,6 @@ class ProviderPair(EmbeddedMongoModel):
     role = fields.IntegerField(min_value=Roles.READ, max_value=Roles.ADMIN, default=Roles.ADMIN)
 
     def to_front_dict(self) -> dict:
-        return {ProviderPair.ID_FIELD: self.user.get_id(), ProviderPair.ROLE_FIELD: self.role}
+        front = self.user.to_front_dict()
+        front[ProviderPair.ROLE_FIELD] = self.role
+        return front
